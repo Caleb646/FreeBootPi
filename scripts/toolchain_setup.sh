18870a67d7c9e06b0259b45b20f4c5f872294b20
Caleb646 <calebthomas646@yahoo.com>
2024-04-15 09:25:38 -0400

    fixed toolchain script

--- scripts/toolchain_setup.sh
+++ scripts/toolchain_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -e
 
+set -e
 echo "Downloading and Installing x86_64 ARM Toolchain to $PWD"
-wget --quiet https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-elf.tar.xz?rev=a05df3001fa34105838e6fba79ee1b23&hash=DCB97F9E407955B162E4652F4E78B6CCDF75E4FF
-tar -xf 'arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-elf.tar.xz?rev=a05df3001fa34105838e6fba79ee1b23'
-mv "arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-elf" "arm_toolchain"
-rm 'arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-elf.tar.xz?rev=a05df3001fa34105838e6fba79ee1b23'
+curl -o $PWD/toolchain.tar.xz -OL https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-elf.tar.xz?rev=a05df3001fa34105838e6fba79ee1b23&hash=DCB97F9E407955B162E4652F4E78B6CCDF75E4FF
+
+wait
+
+tar -xf $PWD/toolchain.tar.xz
+mv $PWD/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf "$PWD/arm_toolchain"
+rm $PWD/toolchain.tar.xz
