d3467fd05c5355e3b0c53e6a51ba36a5fbc22fee
Caleb646 <calebthomas646@yahoo.com>
2024-04-24 18:16:15 -0400

    completed sender

--- sender/sender.py
+++ sender/sender.py
@@ -12,6 +12,23 @@ if __name__ == "__main__":
     ser = serial.Serial(
         args.port, args.baud, timeout=0, parity=serial.PARITY_NONE
         )
-    
     assert ser.is_open, f"Failed to open serial port: {args.port}"
-    assert os.path.isfile(args.kpath), f"Kernel path given is not a valid file: [{args.kpath}]"
+    assert os.path.isfile(args.kpath), f"Given Kernel path is not a valid file: [{args.kpath}]"
+
+    breaks = 0
+    while True:
+        data = ser.read_all()
+        if breaks >= 3:
+            break
+        if data:
+            for b in data:
+                if b == "\x03":
+                    breaks += 1
+                if b != "\x03" and breaks > 0:
+                    breaks = 0
+    print("Received 3 breaks. Sending the Kernel")
+    with open(args.kpath, mode="rb") as kfile:
+        file_size = len(kfile)
+        ser.write(file_size.to_bytes())
+        kbytes_sent = ser.write(kfile)
+        assert file_size == kbytes_sent, f"Failed to send the entire kernel"
