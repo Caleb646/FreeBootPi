f92929e1535b608b61869f667c5422a35bd7f98e
Caleb646 <calebthomas646@yahoo.com>
2024-05-09 13:52:40 -0400

    Added unity testing

--- tests/tests/test_mem.c
+++ tests/tests/test_mem.c
@@ -1,13 +1,12 @@
-
 #include "unity.h"
-#include "mem.h"
+//#include "mem.h"
 
-void setup(void)
+void setUp(void)
 {
     // set stuff up here
 }
 
-void tear_down(void)
+void tearDown(void)
 {
     // clean stuff up here
 }
@@ -17,9 +16,8 @@ void test_malloc(void)
     TEST_ASSERT_TRUE(1 == 1);
 }
 
-
-// not needed when using generate_test_runner.rb
-int main(void) {
+int main(void)
+{
     UNITY_BEGIN();
     RUN_TEST(test_malloc);
     return UNITY_END();
